4da475a26ec3daf7b8be774d326fbbb0822c3579
JerinPaulS <jerinsprograms@gmail.com>
2025-01-23 20:45:52 +0530

    Daily challenge 23 Jan 25

---
+++ CP/src/Leetcode/DailyChallenge/CountServersthatCommunicate_1267.java
@@ -0,0 +1,32 @@
+package Leetcode.DailyChallenge;
+
+public class CountServersthatCommunicate_1267 {
+
+    public int countServers(int[][] grid) {
+        int rows = grid.length;
+        int cols = grid[0].length;
+        int[] rowsCount = new int[rows];
+        int[] colsCount = new int[cols];
+
+        for (int row=0; row<rows; row++) {
+            for (int col=0; col<cols; col++) {
+                if (grid[row][col] == 1) {
+                    rowsCount[row]++;
+                    colsCount[col]++;
+                }
+            }
+        }
+        int result = 0;
+
+        for (int row=0; row<rows; row++) {
+            for (int col=0; col<cols; col++) {
+                if (grid[row][col] == 1 && (rowsCount[row] > 1 || colsCount[col] > 1)) {
+                    result++;
+                }
+            }
+        }
+
+        return result;
+    }
+
+}
