275c10e47265840a178690aa64ce91214e6214aa
JerinPaulS <jerinsprograms@gmail.com>
2025-02-06 21:36:29 +0530

    Feb 6 Daily challenge

---
+++ CP/src/Leetcode/DailyChallenge/YR2025/Tuple_with_Same_Product_1726.java
@@ -0,0 +1,26 @@
+package Leetcode.DailyChallenge.YR2025;
+
+import java.util.HashMap;
+
+public class Tuple_with_Same_Product_1726 {
+
+    public int tupleSameProduct(int[] nums) {
+        HashMap<Integer, Integer> nums_freq = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                int key = nums[i] * nums[j];
+                nums_freq.put(key, nums_freq.getOrDefault(key, 0) + 1);
+            }
+        }
+
+        int result = 0;
+        for (int key : nums_freq.keySet()) {
+            int value = nums_freq.get(key);
+            result += (8 * (((value - 1) * value) / 2));
+        }
+        return result;
+
+    }
+
+}
