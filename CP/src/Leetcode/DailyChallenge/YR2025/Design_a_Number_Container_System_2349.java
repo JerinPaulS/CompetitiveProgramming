c81c89cc7222ca185332c63fcbc4782d7a07888a
JerinPaulS <jerinsprograms@gmail.com>
2025-02-10 21:01:02 +0530

    Feb 8 Daily Challenge

---
+++ CP/src/Leetcode/DailyChallenge/YR2025/Design_a_Number_Container_System_2349.java
@@ -0,0 +1,33 @@
+package Leetcode.DailyChallenge.YR2025;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+
+public class Design_a_Number_Container_System_2349 {
+
+    Map<Integer,Integer> m;
+    Map<Integer, PriorityQueue<Integer>> d;
+
+    public Design_a_Number_Container_System_2349() {
+        m = new HashMap<>();
+        d = new HashMap<>();
+    }
+
+    public void change(int i, int n) {
+        if(m.containsKey(i) && m.get(i)==n)
+            return;
+        m.put(i,n);
+        d.computeIfAbsent(n, k->new PriorityQueue<>()).offer(i);
+    }
+
+    public int find(int n) {
+        if(!d.containsKey(n))
+            return -1;
+        PriorityQueue<Integer> pq = d.get(n);
+        while(!pq.isEmpty() && m.get(pq.peek()) != n)
+            pq.poll();
+        return pq.isEmpty() ? -1 : pq.peek();
+    }
+
+}
