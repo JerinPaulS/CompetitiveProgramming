ef9f9ebeab4f0592ceeeee62fc09da6c45581058
JerinPaulS <jerinsprograms@gmail.com>
2025-02-10 21:02:30 +0530

    Feb 9 Daily challenge

---
+++ CP/src/Leetcode/DailyChallenge/YR2025/Count_Number_of_Bad_Pairs_2364.java
@@ -0,0 +1,20 @@
+package Leetcode.DailyChallenge.YR2025;
+
+import java.util.HashMap;
+
+public class Count_Number_of_Bad_Pairs_2364 {
+
+    public long countBadPairs(int[] nums) {
+        HashMap<Integer, Integer> freq = new HashMap<>();
+        long result = 0;
+        for (int index = 0; index < nums.length; index++) {
+            int num = nums[index];
+            int key = num - index;
+            int goodPair = freq.getOrDefault(key, 0);
+            result += index - goodPair;
+            freq.put(key, goodPair + 1);
+        }
+        return result;
+    }
+
+}
